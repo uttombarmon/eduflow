0e8c96601fd3b67f465749f5bf13ff7d334f3f81
uttombarmon <uttombarmondev@gmail.com>
2026-01-14 03:37:34 +0600

    feat: implement authentication flow with login and signup functionality, add AuthInitializer component

--- lib/features/auth/userApi.ts
+++ lib/features/auth/userApi.ts
@@ -5,20 +5,45 @@ export const userApi = createApi({
   reducerPath: "userApi",
 
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://mocki.io/v1/",
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/`,
     prepareHeaders: (headers) => {
       headers.set("Accept", "application/json");
       return headers;
     },
+    credentials: "include",
   }),
   tagTypes: ["User"],
-
   endpoints: (builder) => ({
+    signup: builder.mutation({
+      query: (userData) => ({
+        url: "signup",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    login: builder.mutation({
+      query: (userData) => ({
+        url: "login",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    checkAuth: builder.mutation({
+      query: () => ({
+        url: "me",
+        method: "GET",
+      }),
+      invalidatesTags: ["User"]
+    }),
     getUserProfile: builder.query<User, void>({
       query: () => "80834fcd-ec2c-4350-abf4-8f433c593327",
-      providesTags: ["User"],
     }),
   }),
 });
 
-export const { useGetUserProfileQuery } = userApi;
+export const {
+  useSignupMutation,
+  useLoginMutation,
+  useCheckAuthMutation,
+  useGetUserProfileQuery,
+} = userApi;
--- providers/Provider.tsx
+++ providers/Provider.tsx
@@ -2,10 +2,15 @@
 import { makeStore } from "@/lib/store";
 import React, { useMemo } from "react";
 import { Provider } from "react-redux";
+import { AuthInitializer } from "./AuthInitializer";
 
 const StateProvider = ({ children }: { children: React.ReactNode }) => {
   const store = useMemo(() => makeStore(), []);
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <AuthInitializer>{children}</AuthInitializer>
+    </Provider>
+  );
 };
 
 export default StateProvider;

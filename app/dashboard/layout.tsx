103fd901c2b695992c6be27535a3c0701a75ce14
uttombarmon <uttombarmondev@gmail.com>
2026-01-21 22:39:27 +0600

    added loading state on dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,17 @@
+"use client"
 import DashboardLayout from "@/components/features/dashboard/DashboardLayout";
+import Loading from "@/components/layout/Loading";
+import { useAppSelector } from "@/lib/hooks";
+import { RootState } from "@/lib/store";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const { user, isCheckingAuth } = useAppSelector((state: RootState) => state.auth);
+    if (isCheckingAuth) {
+        return <div className=" flex items-center justify-center h-screen"><Loading /></div>;
+    }
+    if (!user) {
+        return <div>Unauthorized</div>;
+    }
     return <DashboardLayout>{children}</DashboardLayout>;
 }

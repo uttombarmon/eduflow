4ce179f6f1e9eedb048c6c0211586f72cea077cd
uttombarmon <uttombarmondev@gmail.com>
2026-02-02 22:35:47 +0600

    updated auth-initializer file to manage user state

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,18 +1,28 @@
-"use client"
+"use client";
 import DashboardLayout from "@/components/features/dashboard/DashboardLayout";
 import Loading from "@/components/layout/Loading";
 import { useAppSelector } from "@/lib/hooks";
 import { RootState } from "@/lib/store";
-import { redirect } from "next/navigation";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const { user, isCheckingAuth } = useAppSelector((state: RootState) => state.auth);
-    if (isCheckingAuth) {
-        return <div className=" flex items-center justify-center h-screen"><Loading /></div>;
+  const router = useRouter();
+  const { user, isCheckingAuth } = useAppSelector(
+    (state: RootState) => state.auth,
+  );
+  useEffect(() => {
+    if (!isCheckingAuth && !user) {
+      router.push("/");
     }
-    if (!user) {
-        return redirect('/');
-    }
-    return <DashboardLayout>{children}</DashboardLayout>;
+  }, [isCheckingAuth, user, router]);
+  if (isCheckingAuth) {
+    return (
+      <div className=" flex items-center justify-center h-screen">
+        <Loading />
+      </div>
+    );
+  }
+  if (!user) return null;
+  return <DashboardLayout>{children}</DashboardLayout>;
 }

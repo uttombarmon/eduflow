d7017d407bdd12a540dac30cdfbfad24f6127d4b
uttombarmon <uttombarmondev@gmail.com>
2026-02-02 17:27:00 +0600

    updated nextjs config file

--- next.config.ts
+++ next.config.ts
@@ -26,14 +26,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/v1/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
